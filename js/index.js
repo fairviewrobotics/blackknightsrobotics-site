ab6eb3ff4145ea54c8164123d1c4592e2f9e9438
aaditsangvikar <aadit.sangvikar@gmail.com>
2023-06-27 13:38:31 -0600

    home page animations/words finished

--- js/index.js
+++ js/index.js
@@ -39,7 +39,7 @@ window.addEventListener('scroll',function(){
     }
 
 
-    if (inRange(value, 475, 1600)) {
+    if (inRange(value, 475, 1450)) {
         if (inTwo == false){
             inTwo = true
             changingwords.setAttribute('class', 'changingwords-hide')
@@ -52,6 +52,201 @@ window.addEventListener('scroll',function(){
         
     } else {
         inTwo = false
+    }
+
+    if (inRange(value, 1450, 2405)) {
+        if (inThree == false){
+            inThree = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'CHEERERS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inThree = false
+    }
+
+    if (inRange(value, 2405, 3360)) {
+        if (inFour == false){
+            inFour = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'CODERS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inFour = false
+    }
+
+    if (inRange(value, 3360, 4315)) {
+        if (inFive == false){
+            inFive = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'CREATORS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inFive = false
+    }
+
+    if (inRange(value, 4315, 5270)) {
+        if (inSix == false){
+            inSix = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'DOERS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inSix = false
+    }
+
+    if (inRange(value, 5270, 6225)) {
+        if (inSeven == false){
+            inSeven = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'INNOVATORS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inSeven = false
+    }
+
+    if (inRange(value, 6225, 7180)) {
+        if (inEight == false){
+            inEight = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'KNIGHTS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inEight = false
+    }
+
+    if (inRange(value, 7180, 8135)) {
+        if (inNine == false){
+            inNine = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'LEARNERS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inNine = false
+    }
+
+    if (inRange(value, 8135, 9090)) {
+        if (inTen == false){
+            inTen = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'PLANNERS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inTen = false
+    }
+
+    if (inRange(value, 9090, 10045)) {
+        if (inEleven == false){
+            inEleven = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'TEACHERS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inEleven = false
+    }
+
+    if (inRange(value, 10045, 11000)) {
+        if (inTwelve == false){
+            inTwelve = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'TECHNICIANS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inTwelve = false
+    }
+
+    if (inRange(value, 11000, 11955)) {
+        if (inThirteen == false){
+            inThirteen = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'THINKERS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inThirteen = false
+    }
+
+    if (inRange(value, 11955, 12910)) {
+        if (inFourteen == false){
+            inFourteen = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'WORKERS'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inFourteen = false
+    }
+
+    if (inRange(value, 12910, 13865)) {
+        if (inFifteen == false){
+            inFifteen = true
+            changingwords.setAttribute('class', 'changingwords-hide')
+            setTimeout(() => {
+                changingwords.textContent = 'A TEAM'
+                changingwords.setAttribute("class", "changingwords-show");
+            }, 300)
+            
+        }
+        
+    } else {
+        inFifteen = false
     }
     
 })
