8892a83f81e3b72c69992cffcbe156dc65e25011
aaditsangvikar <aadit.sangvikar@gmail.com>
2023-09-02 01:00:31 -0600

    more form stuff

--- php/joinform.php
+++ php/joinform.php
@@ -1,30 +1,32 @@
 <?php 
 
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
+// error_reporting(-1);
+// ini_set('display_errors', 'On');
+// set_error_handler("var_dump");
 
 if(isset($_POST['submit'])){
-    $to = "aadit.sangvikar@gmail.com"; 
+    
+    // $to = "aadit.sangvikar@gmail.com"; 
 
-    $from = $_POST['email']; 
-    $full_name = $_POST['full_name'];
-    $school = $_POST['school'];
-    $grade = $_POST['grade'];
-    $extra = $_POST['extra'];
+    // $from = $_POST['email']; 
+    // $full_name = $_POST['full_name'];
+    // $school = $_POST['school'];
+    // $grade = $_POST['grade'];
+    // $extra = $_POST['extra'];
 
-    $subject = "Black Knights Join Form Submission";
+    // $subject = "Black Knights Join Form Submission";
     
 
 
     
-    $headers = array("From: aadit.sangvikar@gmail.com",
-        "Reply-To: aadit.sangvikar@gmail.com",
-        "X-Mailer: PHP/" . PHP_VERSION
-    );
-    $headers = implode("\r\n", $headers);
+    // $headers = array("From: aadit.sangvikar@gmail.com",
+    //     "Reply-To: aadit.sangvikar@gmail.com",
+    //     "X-Mailer: PHP/" . PHP_VERSION
+    // );
+    // $headers = implode("\r\n", $headers);
 
-    @mail($to,$subject,$message,$headers);
+    // @mail($to,$subject,$message,$headers);
+    mail("aadit.sangvikar@gmail.com", "Here is the subject line", $_POST["insert your message here"]. "From: an@email.address");
     
    
     
