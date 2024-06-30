f665f2dfbd59d25235e092bc83f08d0fd0473e7e
aaditsangvikar <aadit.sangvikar@gmail.com>
2024-06-30 15:38:36 -0600

    sponsors, calendar, buttons

--- src/components/CalendarSection.jsx
+++ src/components/CalendarSection.jsx
@@ -4,7 +4,19 @@ import "./styles/CalendarSection.css";
 const CalendarSection = () => {
   return (
     <div className="calendar-wrapper">
-      <h1>Team Calendar</h1>
+      <h1>Team Events Calendar</h1>
+      <iframe
+        src="https://embed.styledcalendar.com/#PmvG8YO9BsKhZ1izM7RT"
+        title="Styled Calendar"
+        class="styled-calendar-container"
+        style={{ width: "100%", border: "none", height: "100%" }}
+        data-cy="calendar-embed-iframe"
+      ></iframe>
+      <script
+        async
+        type="module"
+        src="https://embed.styledcalendar.com/assets/parent-window.js"
+      ></script>
     </div>
   );
 };
---
+++ src/components/SponsorList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SponsorList = (props) => {
+  return (
+    <div className="sponsorlist-wrapper">
+      <h1>{props.sponsorType}</h1>
+    </div>
+  );
+};
+
+export default SponsorList;
