e73e38e04671b5c7545c3d973b8b17f8130dfa0a
aaditsangvikar <aadit.sangvikar@gmail.com>
2024-06-30 16:13:56 -0600

    need to redeploy

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../App.css";
 
 const SignUp = () => {
-  return <h1 className="sign-up">SIGN UP</h1>;
+  return <h1 className="sign-upa">SIGN UP</h1>;
 };
 
 export default SignUp;
