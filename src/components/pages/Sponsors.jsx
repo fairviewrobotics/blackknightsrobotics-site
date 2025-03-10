95fcfd298af707f7db8ed2fac7217b3451691eb0
Matthew Spratlin <matthewspratlin@gmail.com>
2025-03-10 17:35:22 -0600

    Added sponsors to sponsors page on the website using SponsorList.jsx and pulling the list of sponsors from sponsors.json

--- src/components/pages/Sponsors.jsx
+++ src/components/pages/Sponsors.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import SponsorHeader from '../SponsorHeader'
+import SponsorList from "../SponsorList";
 
 const Sponsors = () => {
   return (
-    <div><SponsorHeader /></div>
+    <div>
+        <img src="/images/members.jpg" alt="Sponsors" className="sponsor-cover sponsor-img" />
+        <SponsorHeader />
+      <SponsorList/>
+    </div>
   )
 }
 
