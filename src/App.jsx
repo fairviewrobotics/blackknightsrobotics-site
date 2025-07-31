12c6c6e2445786bb6289882169de2582ce00a040
Dan Josh <95506168+ddjoshua01@users.noreply.github.com>
2025-07-31 00:52:48 -0600

    remove vercel stuff

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,9 @@ import Calendar from "./components/pages/Calendar";
 import Robots from "./components/pages/Robots";
 import Sponsors from "./components/pages/Sponsors";
 
-import { Analytics } from "@vercel/analytics/react";
-
 function App() {
   return (
     <>
-      <Analytics />
       <Router>
         <ParallaxProvider>
           <Navbar />
